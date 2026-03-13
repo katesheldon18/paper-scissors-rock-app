477c46b604590f4b3277a1c964c51bc3fe3dd4c3
Kate <katshe18@gmail.com>
2026-03-14 11:20:09 +1300

    chore(backend): init Spring Boot project and configure SQLite

---
+++ backend/src/main/java/com/paperscissorsrock/paperscissorsrock/PaperscissorsrockApplication.java
@@ -0,0 +1,13 @@
+package com.paperscissorsrock.paperscissorsrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperscissorsrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperscissorsrockApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paperscissorsrock/paperscissorsrock/PaperscissorsrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperscissorsrock.paperscissorsrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperscissorsrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
